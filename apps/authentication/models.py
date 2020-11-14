792e3015953cc555f550143bec9cd3999e8c2b5e
AnjalBam <anjalbam81@gmail.com>
2020-11-14 11:43:39 +0545

    [fix] minor field edits and migrations

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -3,18 +3,21 @@ from django.contrib.auth.models import AbstractUser
 
 
 class MyCustomUser(AbstractUser):
-  date_joined = models.DateTimeField(auto_now_add=True)
-  date_modified = models.DateTimeField(auto_now=True)
-  email = models.EmailField(unique=True, db_index=True)
-  username = models.CharField(max_length=255, unique=True, db_index=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique=True, db_index=True)
+    username = models.CharField(max_length=255, unique=True, db_index=True)
 
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
-  user_permissions = None
-  groups = None
-  
+    user_permissions = None
+    groups = None
+
 
 class Profile(models.Model):
-  user = models.OneToOneField(MyCustomUser, on_delete=models.CASCADE)
-  profile_pic = models.ImageField(upload_to='profile_images/')
+    user = models.OneToOneField(MyCustomUser, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_images/')
+    date_of_birth = models.DateField()
+    bio = models.TextField(null=True, blank=True)
+
