64423c504c6473eb4d592c325703d563ec03f1aa
AnjalBam <anjalbam81@gmail.com>
2020-11-14 11:09:29 +0545

    [add] migtation files and update requirement;

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class MyCustomUser(AbstractUser):
+  date_joined = models.DateTimeField(auto_now_add=True)
+  date_modified = models.DateTimeField(auto_now=True)
   email = models.EmailField(unique=True, db_index=True)
+  username = models.CharField(max_length=255, unique=True, db_index=True)
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['username']
@@ -11,3 +14,7 @@ class MyCustomUser(AbstractUser):
   user_permissions = None
   groups = None
   
+
+class Profile(models.Model):
+  user = models.OneToOneField(MyCustomUser, on_delete=models.CASCADE)
+  profile_pic = models.ImageField(upload_to='profile_images/')
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request, *args, **kwargs):
+    return HttpResponse('Register Page')
