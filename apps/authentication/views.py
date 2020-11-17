5840c543318b3a31ed01c7c9796026781113bc79
AnjalBam <anjalbam81@gmail.com>
2020-11-17 20:22:38 +0545

    [feat] vew for registration added.

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,7 +1,30 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import UserRegisterEmailPasswordSerializer, NameDOBSerializer
+from rest_framework.response import Response
 
-# Create your views here.
 
-def register(request, *args, **kwargs):
-    return HttpResponse('Register Page')
+class RegisterView(generics.GenericAPIView):
+    serializer_class = UserRegisterEmailPasswordSerializer
+    name_dob_serializer = NameDOBSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        if 'email' in kwargs:
+            print(kwargs['email'])
+            if kwargs['step'] == 1:
+                serializer = self.name_dob_serializer(data=data)
+                serializer.is_valid(raise_exception=True)
+                serializer.save(email=kwargs['email'])
+            if kwargs['step'] == 2:
+                pass
+        else:
+            print('Hello there')
+            serializer = self.serializer_class(data=data)
+            serializer.is_valid(raise_exception=True)
+
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
