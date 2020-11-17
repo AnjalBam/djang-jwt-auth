ffe64ff4a01cdad7e0f0a541c7bddef2ba1d718c
AnjalBam <anjalbam81@gmail.com>
2020-11-17 20:22:16 +0545

    [add] two endpoints for registration added

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# auth/
 urlpatterns = [
-    path('register/', views.register)
+    path('register/', views.RegisterView.as_view(), name='register_step_1'),
+    path('register/<str:email>/step/<int:step>', views.RegisterView.as_view(),
+         name='register_steps')
 ]
